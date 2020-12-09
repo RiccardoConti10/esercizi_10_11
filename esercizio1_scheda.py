e1dd5bed411c61ec8189304c9b061331ca60a984
RiccardoConti10 <74264750+RiccardoConti10@users.noreply.github.com>
2020-12-09 19:49:13 +0100

    Create esercizio1_scheda.py

---
+++ esercizio1_scheda.py
@@ -0,0 +1,5 @@
+parola = input("inserisci una parola")
+if parola == parola[::-1] :
+    print("la parola  che hai scelto è palindroma")
+elif parola != parola[::-1] :
+    print("la parola che hai scelto è palindroma")
